b28f83cd86a7655cb9d608ef28be1a2e8d411bae
Easwara Vishnu S R <132735765+EaswaraVishnu@users.noreply.github.com>
2023-09-09 21:13:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const link = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+let input = document.querySelector(".input")
+let button = document.querySelector(".qrbutton")
+let image = document.querySelector(".image")
+let text = document.querySelector(".guide")
+
+button.addEventListener("click",function(){
+  let data = input.value;
+  if(data.length > 0){
+    let imgsrc = link+data;
+    image.src = imgsrc;
+    text.textContent = "QR code generated"
+  }
+  else{
+    text.textContent = "No URL found"
+  }
+})
